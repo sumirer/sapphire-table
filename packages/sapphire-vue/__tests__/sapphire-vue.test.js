6eb279cbc31bca9fe83a8a17c7f3399df11bcd6a
hewancheng <1668961992@qq.com>
2024-10-09 17:55:39 +0800

    feat(command): fix run test command,remove old file

--- packages/sapphire-vue/__tests__/sapphire-vue.test.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const sapphireVue = require('..');
-const assert = require('assert').strict;
-
-assert.strictEqual(sapphireVue(), 'Hello from sapphireVue');
-console.info('sapphireVue tests passed');
